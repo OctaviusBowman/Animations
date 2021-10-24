21c0754a51daad309aafa99141d9242c4cb06db5
Octavius Bowman <octaviusbowman@gmail.com>
2021-10-24 18:43:11 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Octavius Bowman on 10/24/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
